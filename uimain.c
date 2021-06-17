3520199140f5b58c9dbece2f3e4f0892e138b61a
student <sschubert@miners.utep.edu>
2021-06-17 15:07:42 -0600

    I still had some code commented out.

--- uimain.c
+++ uimain.c
@@ -30,10 +30,10 @@ int main()
       break;
     case 'c':
       puts("You selected chars:");
-      /*for (char c = 'A'; c < 'D'; c++)
+      for (char c = 'A'; c < 'D'; c++)
 	print_char_11x16(c);
       for (char c = 'A'; c < 'D'; c++)
-      print_char_5x7(c);*/
+	print_char_5x7(c);
       for (char c = 'A'; c < 'D'; c++)
 	print_char_8x12(c);
       break;
