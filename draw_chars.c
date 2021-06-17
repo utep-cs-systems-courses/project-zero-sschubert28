f8746a21e319adf7eb87bccf4264e1e0fdec0c1a
student <sschubert@miners.utep.edu>
2021-06-17 15:05:14 -0600

    Made the print chars print like normal characters.

--- draw_chars.c
+++ draw_chars.c
@@ -4,10 +4,10 @@
 void print_char_11x16(char c)
 {
   c -= 0x20;
-  for (char row = 0; row < 11; row++) {
-    unsigned short rowBits = font_11x16[c][row];
-    for (char col = 0; col < 16; col++) {
-      unsigned short colMask = 1 << (15-col); /* mask to select bit associated with bit */
+  for (char col = 16; col > 0; col--) {
+    unsigned short colMask = 1 << (15-col); /* mask to select bit associated with bit */
+    for (char row = 0; row < 11; row++) {
+      unsigned short rowBits = font_11x16[c][row];
       putchar( (rowBits & colMask) ? '*' : ' ');
     }
     putchar('\n');
